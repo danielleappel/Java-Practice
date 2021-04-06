0b10a7bd3a1cf35cbe7d7c6ccc055c51195f4318
Dani <54872701+danielleappel@users.noreply.github.com>
2021-04-05 21:21:03 -0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+        ArrayList<ArrayList<String>> rows = new ArrayList<>(n);
+        scan.nextLine();
+        
+        for (int i = 0; i < n; i++)
+        {
+            String inp_row = scan.nextLine();
+            rows.add(i, new ArrayList<String>(Arrays.asList(inp_row.split("\\s"))));
+        }
+
+        int q = scan.nextInt();
+        for (int i = 0; i < q; i++)
+        {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            scan.nextLine();
+            
+            if (x <= rows.size() && y < rows.get(x-1).size()){
+                System.out.println(rows.get(x-1).get(y));
+            }
+            else{
+                System.out.println("ERROR!");
+            }
+
+        }
+
+    }
+}
