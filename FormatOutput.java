8377fb122e41c2474c77d5f8e33d032f26c9376f
Dani <54872701+danielleappel@users.noreply.github.com>
2021-04-08 10:20:48 -0400

    Finished formatting output and added io practice

--- FormatOutput.java
+++ FormatOutput.java
@@ -4,6 +4,7 @@ public class FormatOutput {
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
+            
             System.out.println("================================");
             for(int i=0;i<3;i++){
                 String s1=sc.next();
